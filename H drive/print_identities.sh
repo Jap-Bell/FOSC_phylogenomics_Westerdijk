ae66fc5b5307ba65c199d7957cba2fafe3bf7598
J_Bell <113982756+Jap-Bell@users.noreply.github.com>
2024-11-12 11:36:38 +0100

    Delete H drive directory

--- H drive/print_identities.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in ~/h/BLAST/PurOdoPhia_Blast/"$1"_res/"$2"*; 
-do 
-	echo "$file"; 
-	grep "Identities" "$file" | awk '{print $3 "\t" $4}' | cut -d "/" -f1,2 | cut -d "%" -f1 | sed 's/(//g' | sed 's/\//\t/g' | cut -d $'\t' -f2,3 |sort -k1,1 -r
-done
